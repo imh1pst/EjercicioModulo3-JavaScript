452516a9bdc8a2bfa4d6a2bda7d66735e560f55c
Soyhipst <hello.codehipster@outlook.com>
2022-09-14 23:50:48 -0500

    first commit

---
+++ bucle_for.js
@@ -0,0 +1,7 @@
+//Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+// bucle for
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+};
+
---
+++ bucle_while.js
@@ -0,0 +1,11 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while
+//Bucle while
+
+let i = 0;
+let max = 10;
+
+while (i < max) {
+    console.log(i);
+    i++;
+};
+
