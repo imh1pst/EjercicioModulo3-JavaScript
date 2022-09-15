b668239ac3794fbc55ff4a5be7655d2cdd84a2b3
Soyhipst <hello.codehipster@outlook.com>
2022-09-15 00:32:50 -0500

    two commit

---
+++ break.js
@@ -0,0 +1,11 @@
+//  Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+// factorial break
+
+let lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < lista.length; i++) {
+    if(lista[i] > 8) {
+        break;
+    }
+    console.log(lista[i]);
+};
